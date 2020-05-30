7b845086d77316916eaadf047cb437ddedc70256
Terry Thompson <terrylthompsonintx@gmail.com>
2020-05-30 15:34:10 -0500

    Full functionality with array.  No persistance.

--- server.js
+++ server.js
@@ -3,22 +3,43 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-const notes = [];
+var notes = [];
+const shortid = require('shortid');
+const { db } = require('./db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); //Serves html,css
+app.use(express.static(path.join(__dirname, 'public'))); //Serves html,css,js
 
+function readFromDb(){}
+function writeToDb(){}
 
-app.get('/api/notes', (req, res) => {  //returns all notes in the notes array
+app.get('/api/notes', (req, res) => {  //reads from returns all notes in the notes array
+  readFromDb();
   res.json(notes);
 });
-app.post('/api/notes', (req, res) => {  //recieves note and pushes to notes array
-  console.log(req.body);
-  res.send('Hello!');
-});
-app.delete('/api/note', (req, res) => {  //recieves note and deletes from array
-  res.send('Hello!');
+
+app.post('/api/notes', (req, res) => {  //recieves note and pushes to notes array and saves to db file.
+  
+  let newNote = {"title": req.body.title,"text":req.body.text,"id":shortid.generate() }
+  notes.push(newNote);
+  writeToDb();
+  res.sendStatus(200);
+})
+
+app.delete('/api/notes/:id', (req, res) => {  //recieves note id and deletes from array
+  console.log (notes);
+  
+  let newnotes = notes.filter(function(e){
+    return e.id !== req.params.id;
+  });
+
+  notes= newnotes;
+  console.log(notes);
+  writeToDb();
+
+
+  res.sendStatus(200);
 });
 
 
