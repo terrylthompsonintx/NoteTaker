77e550bb0248e223795dddda356c836f038d2c14
Terry Thompson <terrylthompsonintx@gmail.com>
2020-05-30 19:02:03 -0500

    finished, I think

--- server.js
+++ server.js
@@ -4,41 +4,45 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 var notes = [];
+var thestring ='';
 const shortid = require('shortid');
-const { db } = require('./db.json');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); //Serves html,css,js
 
-function readFromDb(){}
-function writeToDb(){}
+function readFromDb(){
+  let db = fs.readFileSync(path.join(__dirname, './db/db.json'));
+  notes = JSON.parse(db);
+  //console.log(notes);
+}
+function writeToDb(){
+  thestring =JSON.stringify(notes);
+  fs.writeFile(path.join(__dirname, './db/db.json'), thestring , function (err) {
+    if (err) throw err;
+    
+  });
+}
 
-app.get('/api/notes', (req, res) => {  //reads from returns all notes in the notes array
+app.get('/api/notes', (req, res) => {  //reads fropm db, coverts to array, returns array
   readFromDb();
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {  //recieves note and pushes to notes array and saves to db file.
-  
   let newNote = {"title": req.body.title,"text":req.body.text,"id":shortid.generate() }
   notes.push(newNote);
   writeToDb();
   res.sendStatus(200);
-})
+});
 
-app.delete('/api/notes/:id', (req, res) => {  //recieves note id and deletes from array
-  console.log (notes);
-  
+app.delete('/api/notes/:id', (req, res) => {  //recieves note id and deletes from array writes new array to db
   let newnotes = notes.filter(function(e){
     return e.id !== req.params.id;
   });
-
   notes= newnotes;
-  console.log(notes);
   writeToDb();
-
-
   res.sendStatus(200);
 });
 
@@ -48,4 +52,3 @@ app.listen(PORT, () => {
   });
 
 
-/* */
